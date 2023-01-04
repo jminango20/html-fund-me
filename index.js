bf7c212dc3f1f9bffb0da305e4a3fefec743a075
jminango20 <jcarlosminango@gmail.com>
2023-01-04 16:45:19 -0300

    Javascript in its own file

---
+++ index.js
@@ -0,0 +1,12 @@
+async function connect(){
+    if(typeof window.ethereum !== "undefined"){
+        try{
+            await window.ethereum.request({method: "eth_requestAccounts"})
+        } catch (error){
+            console.log(error)
+        }
+        document.getElementById("connectButton").innerHTML = "Connected!"
+    }else{
+        document.getElementById("connectButton").innerHTML = "Please install Metamask!"
+    }
+}
