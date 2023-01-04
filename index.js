d67c6e5ff439b84c55579a9371bcaa3cf764d9c7
jminango20 <jcarlosminango@gmail.com>
2023-01-04 18:54:08 -0300

    front end js vs nodejs

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+import { ethers } from "./ethers-5.6.esm.min.js"
+
+const connectButton = document.getElementById("connectButton")
+const fundButton = document.getElementById("fundButton")
+
+connectButton.onclick = connect
+fundButton.onclick = fund
+
+console.log(ethers)
+
 async function connect(){
     if(typeof window.ethereum !== "undefined"){
         try{
@@ -5,8 +15,19 @@ async function connect(){
         } catch (error){
             console.log(error)
         }
-        document.getElementById("connectButton").innerHTML = "Connected!"
+        connectButton.innerHTML = "Connected!"
     }else{
-        document.getElementById("connectButton").innerHTML = "Please install Metamask!"
+        fundButton.innerHTML = "Please install Metamask!"
     }
+}
+
+async function fund(ethAmount){
+    console.log(`Funding with ${ethAmount}...`)
+    if(typeof window.ethereum !== "undefined"){
+        // provider / connection to the blockchain
+        // signer / wallet / someone with some gas
+        // contract that we are interacting 
+        // ^ABI & Address
+    }
+
 }
